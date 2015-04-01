d2b46920c8818be3c5df2011a6d347c64c82be01
LightsOnHudson <brbshaver@gmail.com>
2015-04-01 09:57:08 -0600

    Testing pull script

--- excluded_plugins.inc.php
+++ excluded_plugins.inc.php
@@ -2,4 +2,5 @@
 //put the plugins that should NOT be included in the matrix output
 $EXCLUDE_PLUGIN_ARRAY = array("MatrixMessage","fpp-matrixtools","edmrds","MessageQueue","ProjectorControl","Nagios","LiveMatrix");
 
+//TEST GIT
 ?>
