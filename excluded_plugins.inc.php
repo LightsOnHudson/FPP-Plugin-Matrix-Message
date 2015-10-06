5e099a84774293817c077cc7b3fb95767b204e6b
LightsOnHudson <bshaver@MBPRO-TDL.local>
2015-10-06 17:58:27 -0500

    exclude projector control

--- excluded_plugins.inc.php
+++ excluded_plugins.inc.php
@@ -1,6 +1,6 @@
 <?php
 //put the plugins that should NOT be included in the matrix output
 
-$EXCLUDE_PLUGIN_ARRAY = array("CronEditor","MatrixMessage","fpp-matrixtools","edmrds","MessageQueue");
+$EXCLUDE_PLUGIN_ARRAY = array("ProjectorControl","CronEditor","MatrixMessage","fpp-matrixtools","edmrds","MessageQueue");
 
 ?>
