02f1293b550eacf90309ce9bad2bab1195b7a1bc
LightsOnHudson <bshaver@MBPRO-TDL.local>
2015-10-06 17:57:42 -0500

    Exclude CronEditor

--- excluded_plugins.inc.php
+++ excluded_plugins.inc.php
@@ -1,10 +1,6 @@
 <?php
 //put the plugins that should NOT be included in the matrix output
-<<<<<<< HEAD
-$EXCLUDE_PLUGIN_ARRAY = array("CronEditor","MatrixMessage","fpp-matrixtools","edmrds","MessageQueue");
-=======
-$EXCLUDE_PLUGIN_ARRAY = array("BetaBrite","MatrixMessage","fpp-matrixtools","edmrds","MessageQueue","ProjectorControl","Nagios","LiveMatrix");
->>>>>>> origin/master
 
-//TEST GIT
+$EXCLUDE_PLUGIN_ARRAY = array("CronEditor","MatrixMessage","fpp-matrixtools","edmrds","MessageQueue");
+
 ?>
