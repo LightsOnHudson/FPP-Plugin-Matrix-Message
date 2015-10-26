65ed0aa8a0eacdc9b571aad4ea85af4f974144c5
LightsOnHudson <bshaver@officemini.home.lan>
2015-10-26 06:38:20 -0600

    Exclude BetaBriteMessage from plugin queue

--- excluded_plugins.inc.php
+++ excluded_plugins.inc.php
@@ -1,6 +1,6 @@
 <?php
 //put the plugins that should NOT be included in the matrix output
-$EXCLUDE_PLUGIN_ARRAY = array("BetaBrite","MatrixMessage","fpp-matrixtools","edmrds","MessageQueue","ProjectorControl","Nagios","LiveMatrix");
+$EXCLUDE_PLUGIN_ARRAY = array("BetaBriteMessage","BetaBrite","MatrixMessage","fpp-matrixtools","edmrds","MessageQueue","ProjectorControl","Nagios","LiveMatrix");
 
 //TEST GIT
 ?>
