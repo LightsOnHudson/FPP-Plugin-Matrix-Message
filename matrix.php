ee583721da5202f3ed29b9d6deb49af9077663e7
LightsOnHudson <brbshaver@gmail.com>
2016-12-02 21:53:51 -0700

    high water

--- matrix.php
+++ matrix.php
@@ -165,16 +165,17 @@ if($MESSAGE_QUEUE_PLUGIN_ENABLED) {
         if($messageCount >0 ) {
         //if($queueMessages != null || $queueMessages != "") {
         $MATRIX_ACTIVE = true;
-        $queueCount =0;
+     //   $queueCount =0;
         
         $LOOP_COUNT =0;
         	do {
+        		$queueCount =0;
         		logEntry("LOOP COUNT: ".$LOOP_COUNT++);
         		
         		//extract the high water mark from the first message and write that back to the plugin! or
         		//gets the same message twice in a flood of incomming on demand messages
         		
-        		$messageParts = explode("|",$queueMessages[0]);
+        		$messageQueueParts = explode("|",$queueMessages[0]);
         		logEntry("MATRIX plugin: Writing high water for plugin:".$MATRIX_PLUGIN_OPTIONS." ".urldecode($messageQueueParts[0]));
         		WriteSettingToFile("LAST_READ",urldecode($messageQueueParts[0]),$MATRIX_PLUGIN_OPTIONS);
         		
