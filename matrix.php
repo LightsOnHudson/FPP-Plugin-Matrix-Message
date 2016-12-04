1138e53230fde637fbe34a03093982099d144837
LightsOnHudson <brbshaver@gmail.com>
2016-12-04 16:52:33 -0700

    High Water

--- matrix.php
+++ matrix.php
@@ -176,7 +176,7 @@ if($MESSAGE_QUEUE_PLUGIN_ENABLED) {
         		//gets the same message twice in a flood of incomming on demand messages
         		
         		$messageQueueParts = explode("|",$queueMessages[0]);
-        		logEntry("MATRIX plugin: Writing high water for plugin:".$MATRIX_PLUGIN_OPTIONS." ".urldecode($messageQueueParts[0]));
+        	//	logEntry("MATRIX plugin: Writing high water for plugin:".$MATRIX_PLUGIN_OPTIONS." ".urldecode($messageQueueParts[0]));
         	//	WriteSettingToFile("LAST_READ",urldecode($messageQueueParts[0]),$MATRIX_PLUGIN_OPTIONS);
         		
         		//echo "0: ".$messageParts[0]."\n";
